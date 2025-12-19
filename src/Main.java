24659dc03a9176a4cda906e03541db8d48c24ecf
Ice <aisultan2007q.gmail.com git config --list fwfew>
2025-12-19 09:08:33 +0500

    Hekmatullah Hassanyar , Aisultan Beimbet , Erasyl Baiqonyr

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Clinic clinic = new Clinic("City Clinic");
+        Doctor doctor = new Doctor("Smith");
+        Patient patient = new Patient("Aisultan");
+
+        Appointment appointment =
+                clinic.scheduleAppointment(patient, doctor, "10:00 AM");
+
+        doctor.consult(patient, appointment);
+        clinic.complete(appointment);
+
+        System.out.println(appointment);
+        patient.printHistory();
+    }
+}
+
